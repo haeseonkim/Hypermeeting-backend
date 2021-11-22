76b298632c0dabfa16760889f32d491285fa429c
haeseonkim <khs51777@naver.com>
2021-11-22 15:19:32 +0900

    Initial commit

---
+++ src/main/java/com/example/hypermeetingbackend/HypermeetingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hypermeetingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermeetingBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HypermeetingBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/hypermeetingbackend/HypermeetingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hypermeetingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermeetingBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
